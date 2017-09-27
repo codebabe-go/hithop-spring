c2baf929ea349ebcca7c110f987f78bc3b987387
codebabe <codebabe.go@gmail.com>
2017-09-27 19:31:23 +0800

    mqc加了一个基于springboot的启动

--- server/mqc/src/main/java/me/codebabe/server/mqc/startup/Application.java
+++ server/mqc/src/main/java/me/codebabe/server/mqc/startup/Application.java
@@ -1,12 +1,14 @@
-package me.codebabe.outspace.startup;
+package me.codebabe.server.mqc.startup;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.env.StandardEnvironment;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.io.IOException;
@@ -15,21 +17,19 @@ import java.util.Properties;
 
 /**
  * author: code.babe
- * date: 2017-01-13 11:43
+ * date: 2017-09-27 13:59
  */
 @SpringBootApplication
 @EnableAsync
 @ImportResource({
-        "classpath:config/hithop-outspace.xml",
 })
-@ComponentScan(basePackages = "me.codebabe.outspace")
-public class Application {
+@ComponentScan(basePackages = "me.codebabe.server.mqc")
+public class Application implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
-        application.setBannerMode(Banner.Mode.OFF);
         Properties props = new Properties();
         try {
             props.load(new InputStreamReader(Application.class.getClassLoader().getResourceAsStream("properties/application.properties"), "UTF-8"));
@@ -37,6 +37,14 @@ public class Application {
         } catch (IOException e) {
             logger.error(e.getMessage(), e);
         }
+        application.setWebEnvironment(false); // 非web工程项目
+//        application.setEnvironment(new StandardEnvironment());
+        application.setBannerMode(Banner.Mode.OFF);
         application.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Thread.currentThread().join();
     }
 }
